02ec5dc878666a393ce373b18da90e665ab7428d
Wato Elle <watoelle@gmail.com>
2025-03-24 22:18:26 +0300

    updated navbar

--- liban-company-website/src/components/Navbar.jsx
+++ liban-company-website/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaHome, FaInfoCircle, FaBoxOpen, FaUsers, FaUserFriends, FaPhone, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa"; // Added FaEnvelope for email
 import "../styles/global.css";
 
 const Navbar = () => {
@@ -14,13 +15,18 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <ul>
-        <li><button onClick={() => scrollToSection("home")}>Home</button></li>
-        <li><button onClick={() => scrollToSection("about")}>About</button></li>
-        <li><button onClick={() => scrollToSection("products")}>Products</button></li>
-        <li><button onClick={() => scrollToSection("community")}>Community</button></li>
-        <li><button onClick={() => scrollToSection("team")}>Team</button></li>
-        <li><button onClick={() => scrollToSection("contact")}>Contact</button></li>
+        <li><button onClick={() => scrollToSection("home")}><FaHome /> Home</button></li>
+        <li><button onClick={() => scrollToSection("about")}><FaInfoCircle /> About</button></li>
+        <li><button onClick={() => scrollToSection("products")}><FaBoxOpen /> Products</button></li>
+        <li><button onClick={() => scrollToSection("community")}><FaUsers /> Community</button></li>
+        <li><button onClick={() => scrollToSection("team")}><FaUserFriends /> Team</button></li>
+        <li><button onClick={() => scrollToSection("contact")}><FaPhone /> Contact</button></li>
       </ul>
+      <div className="contact-info">
+        <span><FaMapMarkerAlt />Bakaro Market, Mogadishu-Somalia</span>
+        <span><FaPhone />(252) 611-273-333</span>
+        <span><FaEnvelope />liiban.trading@gmail.com</span> {/* Added Email */}
+      </div>
     </nav>
   );
 };
